7baac5415fc19c98c1d92b7b09002defbebab525
Bas van der Hoek <bvdhoek@zilverline.com>
2018-02-22 00:51:11 +0100

    enzyme tests of bare app with title

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {shallow} from 'enzyme'
+import App from './App'
+import Title from './components/Title'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+  const app = shallow(<App/>)
+  const title = <Title content="Recipes"/>
+
+  it('is wrapped in a div', () => {
+    expect(app).toHaveTagName('div')
+  })
+
+  it('contains a title', () => {
+    expect(app).toContainReact(title)
+  })
+})
